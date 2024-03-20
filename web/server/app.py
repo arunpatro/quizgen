63f5a9e472a47efdddecef433f376da633f975e6
rmshin <50217797+rmshin@users.noreply.github.com>
2024-03-20 21:18:55 +1300

    web: implement quiz gen via openai api integration + load secrets from .env

--- web/server/app.py
+++ web/server/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
+from dotenv import dotenv_values
 import tiktoken
 import fitz  # PyMuPDF
+import dspy
+import pydantic
+import random
 
 
 app = Flask(__name__)
+app.config["OPENAI_API_KEY"] = dotenv_values(".env")["OPENAI_API_KEY"]
 MAX_TOKENS = 3000
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 disallowed_special = set(encoding.special_tokens_set) - {"<|endoftext|>"}
@@ -22,11 +27,11 @@ def extract_text_from_pdf(pdf):
 @app.route("/api/processPdf", methods=["POST"])
 def process_pdf():
     if "pdf" not in request.files:
-        return "No pdf file uploaded", 400
+        return "No pdf file uploaded.", 400
 
     pdf = request.files["pdf"]
     if pdf.mimetype != "application/pdf":
-        return "Not a pdf file", 400
+        return "Not a pdf file.", 400
 
     full_text, text_lengths = extract_text_from_pdf(pdf)
     tokens = encoding.encode(full_text, disallowed_special=disallowed_special)
@@ -50,3 +55,79 @@ def process_pdf():
     }
 
     return response
+
+
+class QuizQuestion(pydantic.BaseModel):
+    question: str
+    options: list[dict]
+    correct_option: int
+
+    def serialize(self):
+        return {
+            "question": self.question,
+            "options": self.options,
+            "correct_option": self.correct_option,
+        }
+
+
+class Quiz(pydantic.BaseModel):
+    items: list[QuizQuestion]
+
+
+class GenerateQuestions(dspy.Signature):
+    """I want to create a quiz from the provided text, that will be used to test my understanding of the subject objetively. Generate 5 meaningful questions based on the provided text."""
+
+    passage = dspy.InputField()
+    question_1 = dspy.OutputField(desc="often between 5 and 15 words")
+    question_2 = dspy.OutputField(desc="often between 5 and 15 words")
+    question_3 = dspy.OutputField(desc="often between 5 and 15 words")
+    question_4 = dspy.OutputField(desc="often between 5 and 15 words")
+    question_5 = dspy.OutputField(desc="often between 5 and 15 words")
+
+
+class SingleMCQ(dspy.Signature):
+    """Generate one correct answer and three incorrect answers for the provided question, based off the given passage."""
+
+    question = dspy.InputField()
+    context = dspy.InputField()
+    correct = dspy.OutputField(desc="the correct answer, often between 1 and 10 words")
+    incorrect_1 = dspy.OutputField(desc="often between 1 and 10 words")
+    incorrect_2 = dspy.OutputField(desc="often between 1 and 10 words")
+    incorrect_3 = dspy.OutputField(desc="often between 1 and 10 words")
+
+
+turbo = dspy.OpenAI(model="gpt-4", api_key=app.config["OPENAI_API_KEY"])
+dspy.settings.configure(lm=turbo)
+generate_questions = dspy.Predict(GenerateQuestions)
+generate_mcq = dspy.Predict(SingleMCQ)
+
+
+def _generate_quiz(passage: str) -> Quiz:
+    questions = generate_questions(passage=passage)
+    quiz_items = []
+    for _, q in questions.items():
+        mcq = generate_mcq(question=q, context=passage)
+        options = [
+            mcq["correct"],
+            mcq["incorrect_1"],
+            mcq["incorrect_2"],
+            mcq["incorrect_3"],
+        ]
+        # add ids and shuffle to avoid bias
+        ids = list(range(1, len(options) + 1))
+        random.shuffle(ids)
+        options = [{"id": opt_id, "text": opt} for (opt_id, opt) in zip(ids, options)]
+        random.shuffle(options)
+        quiz_items.append(
+            QuizQuestion(question=q, options=options, correct_option=ids[0])
+        )
+    return Quiz(items=quiz_items)
+
+
+@app.route("/api/generateQuiz", methods=["POST"])
+def generate_quiz():
+    if "passage" not in request.form:
+        return "No passage provided to generate quiz from.", 400
+
+    quiz = _generate_quiz(request.form["passage"])
+    return [item.serialize() for item in quiz.items]
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -17,6 +17,11 @@ interface PdfData {
   max_tokens: number;
   total_tokens: number;
 }
+interface QuizItem {
+  question: string;
+  options: [{ id: number; text: string }];
+  correct_option: number;
+}
 
 // file upload state
 const [file, setFile] = createSignal<File | null>(null);
@@ -24,8 +29,12 @@ const [fileTooLarge, setFileTooLarge] = createSignal(false);
 // pdf processing state
 const [pdfData, setPdfData] = createSignal<PdfData | null>(null);
 const [pdfProcessing, setPdfProcessing] = createSignal(false);
-// demo quiz state
+// demo state
 const [viewDemo, setViewDemo] = createSignal(false);
+// quiz data state
+const [quizData, setQuizData] = createSignal<[QuizItem] | null>(null);
+const [quizProcessing, setQuizProcessing] = createSignal(false);
+const [quizError, setQuizError] = createSignal(false);
 
 const App: Component = () => {
   let fileInput!: HTMLInputElement;
@@ -39,17 +48,21 @@ const App: Component = () => {
         setFileTooLarge(false);
       }
       setViewDemo(false);
+      // reset quiz state upon file upload
+      setQuizData(null);
+      setQuizError(false);
     }
   });
   // process pdf (extract text) if file uploaded
   createEffect(() => {
     const f = file();
     if (f !== null && f!.size <= MAX_FILE_SIZE) {
+      setPdfData(null);
       setPdfProcessing(true);
       processPdfHandler(f)
         .then((data) => setPdfData(data))
-        .then((_) => setPdfProcessing(false))
-        .catch((_) => setPdfProcessing(false));
+        .then(() => setPdfProcessing(false))
+        .catch(() => setPdfProcessing(false));
     }
   });
   // reset file & pdf state if viewing demo quiz
@@ -70,7 +83,13 @@ const App: Component = () => {
       </p>
       <p>Try a demo quiz to see how it works, or upload a file to generate your own.</p>
       <div class={styles.demoOptions}>
-        <button class={styles.tryDemo} onClick={() => setViewDemo(true)}>
+        <button
+          class={styles.tryDemo}
+          onClick={() => {
+            setViewDemo(true);
+            setQuizData(demo1 as [QuizItem]);
+          }}
+        >
           Try demo quiz
         </button>
         <span class={styles.optionsDivider}>or</span>
@@ -121,35 +140,63 @@ const App: Component = () => {
       <Show when={pdfProcessing()}>
         <div class={styles.loadingMessage}>
           <Spinner />
-          Extracting text from PDF file.
+          Extracting text from PDF...
         </div>
       </Show>
-      <Show when={pdfData() != null}>
+      <Show when={pdfData() != null && quizData() == null}>
         <Show when={pdfData()!.total_tokens > pdfData()!.max_tokens}>
           <div class={styles.warningMessage}>
             Text is too long ({pdfData()!.total_tokens} tokens). Truncating to{' '}
             {pdfData()!.max_tokens} tokens.
           </div>
         </Show>
-        <p class={styles.pdfSuccess}>
+        <p class={`${styles.pdfSuccess}${quizProcessing() ? ` ${styles.disabled}` : ''}`}>
           PDF text successfully parsed. Pages considered:{' '}
           {`${pdfData()!.processed_pages}/${pdfData()!.total_pages}`}.
         </p>
-        <button>Generate quiz</button>
+        <button
+          class={styles.quizControlButton}
+          onClick={() => {
+            setQuizProcessing(true);
+            setQuizError(false);
+            generateQuizHandler(pdfData()!.text)
+              .then((data) => setQuizData(data))
+              .then(() => setQuizProcessing(false))
+              .catch(() => setQuizProcessing(false));
+          }}
+          disabled={quizProcessing()}
+        >
+          Generate questions
+        </button>
+        <Show when={quizProcessing()}>
+          <div class={styles.loadingMessage}>
+            <Spinner />
+            Generating...
+          </div>
+        </Show>
+        <Show when={quizError()}>
+          <div class={styles.errorMessage}>
+            Sorry but the quiz cannot be generated right now. Please try again later or{' '}
+            <a href="emailto:quizgen@robertshin.com">contact us</a> for support.
+          </div>
+        </Show>
       </Show>
-      <Show when={viewDemo()}>
-        {demo1.map((q, idx) => (
-          <>
-            <fieldset class={styles.mcFieldset}>
-              <label class={styles.mcQuestion}>{q.question}</label>
-              {q.options.map((opt) => (
-                <div class={styles.mcOption}>
-                  <input type="radio" id={opt.text} value={opt.text} name={idx.toString()} />
-                  <label for={opt.text}>{opt.text}</label>
-                </div>
-              ))}
-            </fieldset>
-          </>
+      <Show when={quizData() != null}>
+        {quizData()!.map((q, idx) => (
+          <fieldset class={styles.mcFieldset}>
+            <label class={styles.mcQuestion}>{q.question}</label>
+            {q.options.map((opt) => (
+              <div class={styles.mcOption}>
+                <input
+                  type="radio"
+                  id={`q${idx}_${opt.id}`}
+                  value={opt.text}
+                  name={idx.toString()}
+                />
+                <label for={`q${idx}_${opt.id}`}>{opt.text}</label>
+              </div>
+            ))}
+          </fieldset>
         ))}
       </Show>
     </>
@@ -172,6 +219,22 @@ async function processPdfHandler(f: File) {
   }
 }
 
+async function generateQuizHandler(passage: string) {
+  const formData = new FormData();
+  formData.append('passage', passage);
+
+  try {
+    const response = await fetch('/api/generateQuiz', {
+      method: 'POST',
+      body: formData,
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    setQuizError(true);
+  }
+}
+
 function uploadHandler(ev: Event) {
   const file = (ev.target as HTMLInputElement).files![0];
   setFile(file);
@@ -182,7 +245,7 @@ function dropHandler(ev: DragEvent) {
   ev.preventDefault();
 
   if (ev.dataTransfer!.items) {
-    // Use DataTransferItemList interface to access the file(s)
+    // Use DataTransferItemList interface to access the file
     const item = ev.dataTransfer!.items[0];
     // If dropped item isn't a file, reject it
     if (item.kind === 'file') {
@@ -190,7 +253,7 @@ function dropHandler(ev: DragEvent) {
       setFile(file);
     }
   } else {
-    // Use DataTransfer interface to access the file(s)
+    // Use DataTransfer interface to access the file
     const file = ev.dataTransfer!.files[0];
     setFile(file);
   }
